a8f9c9e555323e793c9b66307c2fc433370f9a30
Anuj Rana <Anujranasteam@gmail.com>
2024-03-12 21:04:44 +0530

    todo list angular 17
    todo list with local storage angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,69 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+
+  myNotes: string[] = [];
+  totalNotes: number = 0;
+  edit: boolean = false;
+  editIndex: number = 0;
+  error: string = '';
+
+  constructor() { }
+  ngOnInit(): void {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      let allNotes = localStorage.getItem('my-notes');
+      if (allNotes) {
+        this.myNotes = JSON.parse(allNotes);
+        this.totalNotes = this.myNotes.length;
+      }
+    } else {
+      console.warn('localStorage is not available.');
+    }
+  }
+
+  addNote(note: string) {
+    if (note.length) {
+      this.myNotes.push(note);
+      if (typeof window !== 'undefined' && window.localStorage) {
+        localStorage.setItem('my-notes', JSON.stringify(this.myNotes));
+        this.totalNotes = this.myNotes.length;
+      } else {
+        console.warn('localStorage is not available.');
+      }
+    }
+    else {
+      this.error = '*Field can\'t be empty!'
+    }
+  }
+
+  removeNote(noteIndex: number) {
+    this.myNotes.splice(noteIndex, 1);
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem('my-notes', JSON.stringify(this.myNotes));
+      this.totalNotes = this.myNotes.length;
+    } else {
+      console.warn('localStorage is not available.');
+    }
+  }
+
+  editNote(noteIndex: number, note: string) {
+    if (note.length) {
+      this.editIndex = noteIndex;
+      this.myNotes[noteIndex] = note;
+      if (typeof window !== 'undefined' && window.localStorage) {
+        localStorage.setItem('my-notes', JSON.stringify(this.myNotes));
+      } else {
+        console.warn('localStorage is not available.');
+      }
+      this.edit = false;
+    }
+    else {
+      this.error = '*Field can\'t be empty!'
+    }
+  }
+  openEditNote(noteIndex: number) {
+    this.editIndex = noteIndex;
+    this.edit = true;
+  }
 }
